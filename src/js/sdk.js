946887ac643a431a3ec1efb686dca381653672c0
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2025-11-24 06:05:02 +0300

    Update sdk.js

--- src/js/sdk.js
+++ src/js/sdk.js
@@ -131,10 +131,10 @@ function update_projects(data, section_type) {
   content += '\
   <ul class="nav nav-tabs" id="myTab" role="tablist">\
     <li class="nav-item" role="presentation">\
-      <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true"><b>Products with Delivered Outcomes</b></button>\
+      <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true"><b>Delivered Outcomes</b></button>\
     </li>\
     <li class="nav-item" role="presentation">\
-      <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false"><b>Products in Discovery & R&D</b></button>\
+      <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false"><b>Discovery and R&D</b></button>\
     </li>\
   </ul>';
 
